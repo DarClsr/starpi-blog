6a68b2683b4c504fbf0081154cc2b38940896900
“github <1322677864@qq.com>
2022-01-16 21:34:41 +0800

    edit databasejs

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'strapi_blog'),
       user: env('DATABASE_USERNAME', 'root'),
